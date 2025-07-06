eda37c97fefc57503d24c6ff53ec2699f3641ef4
Mayur Ankushrao <mayurankushrao2004@gmail.com>
2025-07-06 23:49:26 +0530

    Day 2 and 3

--- FirstTest.js
+++ FirstTest.js
@@ -1,1 +1,36 @@
 console.log("This is the first test file");
+console.log("External script");
+
+/* Variables in JavaScript */
+let a = 10; // Assignment
+console.log("The value of a is", a); // Retrieval
+
+a = 50.123; // Re-assignment
+console.log("The value of a is", a);
+
+a = "Tripat"; // example of dynamically typed language
+console.log("The value of a is", a);
+
+const age = 18;
+console.log("Age is", age);
+
+// age = 25;
+// ERROR - Assignment to a const variable
+
+a = "25";
+console.log("The value of a is", a);
+
+var bool1 = true; // 1 -> true
+var bool2 = false; // 0 -> false
+
+console.log("Value of bool1", bool1);
+console.log("Value of bool2", bool2);
+
+var b;
+console.log("The value of b is", b); // JavaScript has set it to undefined
+
+var c = null;
+console.log("The value of c is", c);
+
+
+
---
+++ ThirdTest.js
@@ -0,0 +1,88 @@
+//********************************************************************
+// **************************************************************************************** */
+
+// Functions -> reusable blocks of code
+// a complex data type
+
+// 3 Ways
+//  -> Function Declaration using the function keywork
+// -> Function Expression -> const
+//                        -> arrow (ES6)
+
+// Function declaration
+// SYNTAX
+// function 'name_of_the_function'(){
+//  resuable code
+// }
+
+function greet() {
+  console.log("Hello World");
+}
+
+// Function call
+greet();
+
+// Function Expression
+// SYNTAX
+// const "name_of_the_func" = function(){
+// resuable code
+// }
+
+const greetFunc = function () {
+  console.log("Function expression called");
+};
+
+greetFunc();
+
+// Arrow Function => ES6
+// SYNTAX
+// const "name_of_the_func" = () => {
+// reusable code
+// }
+
+const greetArrow = () => {
+  console.log("Arrow function");
+};
+greetArrow();
+
+// Functiom definition -> inputs -> arguments
+const avgThreeNumber = (num1, num2, num3) => {
+  const avgNum = (num1 + num2 + num3) / 3;
+  return avgNum;
+};
+
+// Function call -> Parameters
+const output = avgThreeNumber(3, 4, 5);
+console.log(output);
+
+// Create a function called greetUser that takes
+// name as input and prints "Hello name"
+const greetUser = (name) => console.log("Hello", name);
+const greetUser2 = (name) => {
+  console.log("Hello", name);
+};
+greetUser("Tripat");
+
+// Create a function that takes a number as
+// an argument and return whether it is even
+// or odd
+
+const evenodd = (num1) => {
+  if (num1 % 2 == 0) {
+    console.log("number is even");
+  } else {
+    console.log("number is odd");
+  }
+};
+
+evenodd(5);
+
+const isEvenOrOdd = (num) => (num % 2 === 0 ? "even" : "odd");
+
+console.log(isEvenOrOdd(5));
+
+// Create a function that takes username and password as arguments
+// if username = "test" password="test123" -> Login successful
+//  username incorrect -> Username is incorrect
+//  password incorrect -> password is incorrect
+// both incorrect -> both are incorrect
