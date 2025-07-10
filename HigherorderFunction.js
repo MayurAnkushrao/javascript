d2a5972529f9b114599819717b50a5d9cf49e201
Mayur Ankushrao <mayurankushrao2004@gmail.com>
2025-07-10 22:57:20 +0530

    definations

--- HigherorderFunction.js
+++ HigherorderFunction.js
@@ -1,2 +1,7 @@
 //HIGHER ORDER FUNCTIONS -> forEach, maop, filter, reduce, sort
-//
+//a function that takes another function as an argument or returns a function as a result is called a higher order function
+//forEach -> executes a provided function once for each array element
+//map -> creates a new array with the results of calling a provided function on every element in the calling array
+//filter -> creates a new array with all elements that pass the test implemented by the provided function
+//reduce -> executes a reducer function (that you provide) on each element of the array, resulting in a single output value
+//callback function -> a function that is passed as an argument to another function and is executed inside that function
